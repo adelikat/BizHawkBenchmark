8d95b04e0093fdb6b2c28b19074578f40220b54e
adelikat <adelikat@tasvideos.org>
2023-05-22 19:25:13 -0500

    set up benchmarks

---
+++ MovieRunner.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using BizHawk.Client.Common;
+using BizHawk.Emulation.Common;
+using BizHawkBenchmark.Noops;
+
+namespace BizHawkBenchmark
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class MovieRunner
+    {
+        private static readonly string[] NesButtons = { "Up", "Down", "Left", "Right", "Select", "Start", "A", "B" };
+
+        private static readonly MovieConfig MovieConfig = new MovieConfig
+        {
+            MovieEndAction = MovieEndAction.Finish,
+            EnableBackupMovies = false,
+            MoviesOnDisk = false,
+            MovieCompressionLevel = 2,
+            VBAStyleMovieLoadState = false,
+            DefaultTasStateManagerSettings = new ZwinderStateManagerSettings()
+        };
+
+        private readonly IMovieSession _movieMovieSession;
+        private readonly IEmulator _emulator;
+        private readonly IController _controller;
+
+        private static void Noop()
+        {
+        }
+
+        public MovieRunner()
+        {
+            var definition = new BenchmarkControllerDefinition(NesButtons);
+            _emulator = new BenchmarkEmulator(definition);
+            _movieMovieSession = new MovieSession(MovieConfig, ".", new NoopDialogParent(), new NoopQuickBmpFile(), Noop, Noop);
+            var bk2 = new Bk2Movie(_movieMovieSession, "test123.bk2");
+            _movieMovieSession.QueueNewMovie(bk2, true, _emulator.SystemId, new Dictionary<string, string>());
+            _movieMovieSession.RunQueuedMovie(true, _emulator);
+            _controller = new Controller(definition);
+            var adapter = new AutoFireStickyXorAdapter { Source = _controller };
+            _movieMovieSession.MovieIn = adapter;
+        }
+
+        [Benchmark]
+        public void RecordFrames()
+        {
+            for (var i = 0; i < 1; i++)
+            {
+                _movieMovieSession.HandleFrameBefore();
+                _emulator.FrameAdvance(_controller, true);
+                _movieMovieSession.HandleFrameAfter();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,43 +1,15 @@
 ﻿// Assumes BizHawk is cloned in a folder next to this one
 
-using System.Collections.Generic;
-using BizHawk.Client.Common;
-using BizHawkBenchmark.Noops;
+using BenchmarkDotNet.Running;
 
-public static class Program
+namespace BizHawkBenchmark
 {
-    private static readonly string[] NesButtons = { "Up", "Down", "Left", "Right", "Select", "Start", "A", "B" };
-
-    public static void Main(string[] args)
+    public static class Program
     {
-        var movieConfig = new MovieConfig
+        public static void Main(string[] args)
         {
-            MovieEndAction = MovieEndAction.Finish,
-            EnableBackupMovies = false,
-            MoviesOnDisk = false,
-            MovieCompressionLevel = 2,
-            VBAStyleMovieLoadState = false,
-            DefaultTasStateManagerSettings = new ZwinderStateManagerSettings()
-        };
-
-        var definition = new BenchmarkControllerDefinition(NesButtons);
-        var emulator = new BenchmarkEmulator(definition);
-
-        var movieSession = new MovieSession(movieConfig, ".", new NoopDialogParent(), new NoopQuickBmpFile(), Noop, Noop);
-        var bk2 = new Bk2Movie(movieSession, "test123.bk2");
-        movieSession.QueueNewMovie(bk2, true, emulator.SystemId, new Dictionary<string, string>());
-        movieSession.RunQueuedMovie(true, emulator);
-        var controller = new Controller(definition);
-        var adapter = new AutoFireStickyXorAdapter { Source = controller };
-        movieSession.MovieIn = adapter;
-        movieSession.HandleFrameBefore();
-        emulator.FrameAdvance(controller, true);
-        movieSession.HandleFrameAfter();
-        var test = movieSession.Movie.GetLogEntries();
-    }
-
-    private static void Noop()
-    {
+            BenchmarkRunner.Run<MovieRunner>();
+        }
     }
 }
 
